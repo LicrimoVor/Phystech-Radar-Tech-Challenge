48c49546a6e23b4dcdf325793086a724987d7866
licrimovor <mr.licrimovor@yandex.ru>
2024-04-28 23:39:13 +0800

    readme

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ time_start = time.time()
 from graph import Graph, NodeFactory, Theory
 from definition_GT import HeuristicDefGT
 
-file = open("data/input_with_weights.csv")
+file = open("data/matrix.txt")
 connections = file.readlines()
 weight = [float(i) for i in connections[-1].split(",")]
 connections = connections[-2::-1][::-1]
@@ -21,8 +21,8 @@ nodes = node_factory.build(weight)
 global_theoryes: dict[int, tuple[Theory, float]] = {}
 graph = Graph(nodes, connections)
 
-for i in range(1, 10):
-    graph.calculate_priority(i / 10, i / 25)
+for i in range(1, 5):
+    graph.calculate_priority(i / 5, i / 12)
 
     for j in range(10):
         def_global_theory = HeuristicDefGT(graph, j)
@@ -35,10 +35,19 @@ for i in range(1, 10):
             global_theoryes[theory.unique_hash] = theory, theory.summ_weight
 
 print("*\t" + "".join([f"TH{i + 1}\t" for i in range(len(connections))]))
-for index, GH in enumerate(list(sorted(global_theoryes.values(), key=lambda _gh: _gh[1], reverse=True))[:5]):
-    print(f"GH{index + 1}\t" + "".join([
-        str(int((i + 1) in list(map(lambda node: node.indx, list(GH[0]))))) + "\t"
-        for i in range(len(connections))
-    ]) + str(GH[1]))
+for index, GH in enumerate(
+    list(sorted(global_theoryes.values(), key=lambda _gh: _gh[1], reverse=True))[:5]
+):
+    print(
+        f"GH{index + 1}\t"
+        + "".join(
+            [
+                str(int((i + 1) in list(map(lambda node: node.indx, list(GH[0])))))
+                + "\t"
+                for i in range(len(connections))
+            ]
+        )
+        + str(GH[1])
+    )
 time_end = time.time()
 print(time_end - time_start)
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 # Команда Бананчики
 
+![[assets/image.png]]
+
+
 #### Запуск кода
 1. Устанавливаем [python3.9](https://www.python.org/downloads/release/python-390/)
 2. В корне проекта выполняем команду ```python .\main.py```
